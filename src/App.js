2f5568cff2cb4b12692575b09a73ea8452a06376
Eglė Juočienė <e.velykyte@gmail.com>
2022-09-11 13:59:39 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Dice from './Dice';
+import Confetti from 'react-confetti';
+import One from './images/One.svg';
+import Two from './images/Two.svg';
+import Three from './images/three.svg';
+import Four from './images/four.svg';
+import Five from './images/five.svg';
+import Six from './images/six.svg';
 
 function App() {
+	const imgArray = [ One, Two, Three, Four, Five, Six ];
+	const generateNewDie = (i) => {
+		const randomValue = randomDiceValue();
+		return {
+			value: randomValue,
+			img: imgArray[randomValue - 1],
+			isHeld: false,
+			id: i + 1
+		};
+	};
 	const allNewDice = () => {
 		const newArray = [];
 		for (let i = 0; i < 10; i++) {
-			const newDice = {
-				value: randomDiceValue(),
-				chosen: false,
-				id: i + 1
-			};
-			newArray.push(newDice);
+			newArray.push(generateNewDie(i));
 		}
 		return newArray;
 	};
@@ -20,11 +32,12 @@ function App() {
 	};
 	const [ dice, setDice ] = useState(allNewDice());
 	const [ tenzies, setTenzies ] = useState(false);
+	const [ rolls, setRolls ] = useState(0);
 
 	useEffect(
 		() => {
 			const firstValue = dice[0].value;
-			const allChosen = dice.every((each) => each.chosen);
+			const allChosen = dice.every((each) => each.isChosen);
 			const allSameNumber = dice.every((each) => each.value === firstValue);
 			if (allChosen && allSameNumber) {
 				setTenzies(true);
@@ -34,34 +47,47 @@ function App() {
 	);
 
 	const chooseDice = (id) => {
-		console.log('clicked');
 		setDice((prevDice) =>
 			prevDice.map((dice) => {
-				return dice.id === id ? { ...dice, chosen: !dice.chosen } : dice;
+				return dice.id === id ? { ...dice, isChosen: !dice.isChosen } : dice;
 			})
 		);
 	};
 	const rollAvailableDice = () => {
 		if (!tenzies) {
-			setDice((oldDice) =>
-				oldDice.map((each, i) => (each.chosen ? each : { value: randomDiceValue(), chosen: false, id: i + 1 }))
-			);
+			let roll = setInterval(() => {
+				setDice((oldDice) => oldDice.map((each, i) => (each.isChosen ? each : generateNewDie(i))));
+			}, 20);
+			setTimeout(() => {
+				clearInterval(roll);
+			}, 100);
+			setRolls((prevRolls) => prevRolls + 1);
 		} else {
 			setDice(allNewDice());
 			setTenzies(false);
+			setRolls(0);
 		}
 	};
+
 	return (
 		<div className="App">
+			{tenzies && <Confetti />}
 			<header className="App-header">Tenzies</header>
 			<p className="text">
 				Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
 			</p>
 			<div className="dice-container">
 				{dice.map((each) => {
-					return <Dice key={each.id} {...each} onClick={() => chooseDice(dice.id)} />;
+					return <Dice key={each.id} {...each} chooseDice={() => chooseDice(each.id)} />;
 				})}
 			</div>
+			{tenzies && <h1 className="bold">Congrats!</h1>}
+			<p className="text">
+				{tenzies && 'Total '}Rolls: {rolls}{' '}
+			</p>
+			<button className="btn" onClick={rollAvailableDice}>
+				{tenzies ? 'Play again' : 'Roll'}
+			</button>
 		</div>
 	);
 }
--- src/Dice.js
+++ src/Dice.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Dice = (props) => {
-	const styles = { backgroundColor: props.chosen ? '#c8553d' : '#ffd5c2' };
+	const filterNotCosen = 'invert(85%) sepia(33%) saturate(3687%) hue-rotate(311deg) brightness(104%) contrast(77%)';
+	const filterChosen = 'invert(39%) sepia(56%) saturate(699%) hue-rotate(323deg) brightness(96%) contrast(90%)';
+	const styles = { filter: props.isChosen ? filterChosen : filterNotCosen };
 	return (
-		<div className="dice-face" style={styles}>
-			<h2 className="dice-number">{props.value}</h2>
+		<div className="dice-face" style={styles} onClick={props.chooseDice}>
+			<img src={props.img} className="dice-img" alt={props.value} />
+			<h2 className="dice-number">{/* {props.value} */}</h2>
 		</div>
 	);
 };
